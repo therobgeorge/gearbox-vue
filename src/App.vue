a98d2cfd84696e75d6508e7810a112063d3dcf20
therobgeorge <georg1rd@gmail.com>
2021-12-29 16:07:19 -0500

    adds sign up, log in and log out views

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <div id="nav">
       <router-link to="/">Home</router-link>
       |
-      <router-link to="/about">About</router-link>
+      <router-link to="/signup">Sign Up</router-link>
+      |
+      <router-link to="/login">Login</router-link>
+      |
+      <router-link to="/logout">Logout</router-link>
     </div>
     <router-view />
   </div>
---
+++ src/views/Signup.vue
@@ -0,0 +1,57 @@
+<template>
+  <form v-on:submit.prevent="submit()">
+    <div class="form-group">
+      <label>Name:</label>
+      <input type="text" class="form-control" v-model="newUserParams.name" placeholder="Name" />
+    </div>
+    <div class="form-group">
+      <label>Email:</label>
+      <input type="email" class="form-control" v-model="newUserParams.email" placeholder="Email Address" />
+    </div>
+    <div class="form-group">
+      <label>Password:</label>
+      <input type="password" class="form-control" v-model="newUserParams.password" placeholder="Password" />
+    </div>
+    <div class="form-group">
+      <label>Confrim Password:</label>
+      <input
+        type="password"
+        class="form-control"
+        v-model="newUserParams.password_confirmation"
+        placeholder="Password"
+      />
+    </div>
+    <div class="form-group">
+      <label>Phone Number (optional):</label>
+      <input type="text" class="form-control" v-model="newUserParams.phone_number" placeholder="Phone Number" />
+    </div>
+    <input type="submit" class="btn btn-primary" value="Submit" />
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      newUserParams: {},
+      errors: [],
+    };
+  },
+
+  methods: {
+    submit: function () {
+      axios
+        .post("/users", this.newUserParams)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
