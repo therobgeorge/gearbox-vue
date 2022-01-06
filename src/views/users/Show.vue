178942ef40b3a1cb75f09a20e9cb499d5dc00c2e
therobgeorge <georg1rd@gmail.com>
2022-01-06 14:17:48 -0500

    Adds pdf report generation to user show

--- src/views/users/Show.vue
+++ src/views/users/Show.vue
@@ -5,9 +5,13 @@
       <div class="html_to_pdf">
         <img v-if="gear.images.length >= 1" :src="gear.images[0].photo_url" />
         <p>
-          {{ gear.category }} {{ gear.make }} {{ gear.model }} {{ gear.color }} {{ gear.serial_number }}
-          {{ gear.other_info }} {{ gear.registered }} {{ gear.missing }}
+          Category: {{ gear.category }} Make: {{ gear.make }} Model: {{ gear.model }} Color: {{ gear.color }} Serial
+          Number: {{ gear.serial_number }} Other Info: {{ gear.other_info }} Registered: {{ gear.registered }} Missing:
+          {{ gear.missing }}
         </p>
+        <p>Add to report?</p>
+        <input type="checkbox" v-model="gear.report" value="true" />
+        <br />
         <router-link :to="`/gears/${gear.id}/edit`">Edit Gear</router-link>
       </div>
     </div>
@@ -19,12 +23,13 @@
 <script>
 import axios from "axios";
 import jsPDF from "jspdf";
-import html2canvas from "html2canvas";
+import "jspdf-autotable";
 
 export default {
   data: function () {
     return {
       user: {},
+      reportItems: [],
     };
   },
   created: function () {
@@ -34,14 +39,34 @@ export default {
     });
   },
   methods: {
+    addToReport: function () {},
     generateReport: function () {
-      window.html2canvas = html2canvas;
-      let doc = new jsPDF();
-      doc.html(document.querySelector(".html_to_pdf"), {
-        callback: function (pdf) {
-          pdf.save("missingitems.pdf");
-        },
+      (this.reportItems = []),
+        this.user.gears.forEach((gear) => {
+          if (gear.report === true) {
+            this.reportItems.push(gear);
+          }
+        });
+      const columns = [
+        { title: "Make", dataKey: "make" },
+        { title: "Model", dataKey: "model" },
+        { title: "Color", dataKey: "color" },
+        { title: "Serial Number", dataKey: "serial_number" },
+        { title: "Other Info", dataKey: "other_info" },
+      ];
+      const doc = new jsPDF({
+        orientation: "portrait",
+        unit: "in",
+        format: "letter",
       });
+      doc.setFontSize(16).text("Missing Items", 0.5, 1.0);
+      doc.setLineWidth(0.01).line(0.5, 1.1, 8.0, 1.1);
+      doc.autoTable({
+        columns,
+        body: this.reportItems,
+        margin: { left: 0.5, top: 1.25 },
+      });
+      doc.save(`Missing Items.pdf`);
     },
   },
 };
